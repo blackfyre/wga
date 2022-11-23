b9811f5cbb69507d16d02e373c9ef0bed4ecde06
Miklós Galicz <gnick666@gmail.com>
2022-11-23 14:04:34 +0100

    feat: carbon design preprocessor for faster build times

--- svelte.config.js
+++ svelte.config.js
@@ -1,13 +1,17 @@
 import adapter from '@sveltejs/adapter-static';
 import preprocess from 'svelte-preprocess';
+import { optimizeImports } from 'carbon-preprocess-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://github.com/sveltejs/svelte-preprocess
 	// for more information about preprocessors
-	preprocess: preprocess({
-		preserve: ['ld+json']
-	}),
+	preprocess: [
+		preprocess({
+			preserve: ['ld+json']
+		}),
+		optimizeImports()
+	],
 	kit: {
 		adapter: adapter({
 			// default options are shown. On some platforms
