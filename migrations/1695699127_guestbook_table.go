e8a856409e81967cae0c97a8d6f3d697d7bc3a24
Miklós Galicz <gnick666@gmail.com>
2023-09-26 15:01:44 +0200

    Refactor guestbook table migration to use collection model and schema fields instead of direct queries

--- migrations/1695699127_guestbook_table.go
+++ migrations/1695699127_guestbook_table.go
@@ -2,26 +2,52 @@ package migrations
 
 import (
 	"github.com/pocketbase/dbx"
+	"github.com/pocketbase/pocketbase/daos"
 	m "github.com/pocketbase/pocketbase/migrations"
+	"github.com/pocketbase/pocketbase/models"
+	"github.com/pocketbase/pocketbase/models/schema"
 )
 
 func init() {
 	m.Register(func(db dbx.Builder) error {
-		// add up queries...
-		columns := map[string]string{
-			"id":       "text",
-			"created":  "text",
-			"updated":  "text",
-			"message":  "text",
-			"name":     "text",
-			"email":    "text",
-			"location": "text",
-		}
+		dao := daos.New(db)
 
-		q := db.CreateTable("guestbook", columns)
-		_, err := q.Execute()
+		collection := &models.Collection{}
 
-		return err
+		collection.Name = "guestbook"
+		collection.Type = models.CollectionTypeBase
+		collection.System = false
+		collection.MarkAsNew()
+		collection.Schema = schema.NewSchema(
+			&schema.SchemaField{
+				Id:      "guestbooks_message",
+				Name:    "message",
+				Type:    schema.FieldTypeText,
+				Options: &schema.TextOptions{},
+			},
+			&schema.SchemaField{
+				Id:          "guestbooks_name",
+				Name:        "name",
+				Type:        schema.FieldTypeText,
+				Options:     &schema.TextOptions{},
+				Presentable: true,
+			},
+			&schema.SchemaField{
+				Id:          "guestbooks_email",
+				Name:        "email",
+				Type:        schema.FieldTypeEmail,
+				Options:     &schema.EmailOptions{},
+				Presentable: true,
+			},
+			&schema.SchemaField{
+				Id:      "guestbooks_location",
+				Name:    "location",
+				Type:    schema.FieldTypeText,
+				Options: &schema.TextOptions{},
+			},
+		)
+
+		return dao.SaveCollection(collection)
 	}, func(db dbx.Builder) error {
 		// add down queries...
 
