9f3de702ddd582b4e4e402d995adc269b1abbe9e
Miklós Galicz <gnick666@gmail.com>
2023-09-26 15:01:13 +0200

    Implement Artist model and migration, including JSON unmarshalling and saving artist data from a JSON file into the database

--- migrations/1695699036_artists_table.go
+++ migrations/1695699036_artists_table.go
@@ -1,13 +1,125 @@
 package migrations
 
 import (
+	"encoding/json"
+	"os"
+
 	"github.com/pocketbase/dbx"
+	"github.com/pocketbase/pocketbase/daos"
 	m "github.com/pocketbase/pocketbase/migrations"
+	"github.com/pocketbase/pocketbase/models"
+	"github.com/pocketbase/pocketbase/models/schema"
 )
+
+type Artist struct {
+	Id     string `json:"id"`
+	Name   string `json:"name"`
+	Source struct {
+		Artist     string `json:"artist"`
+		BirthData  string `json:"birth_data"`
+		Profession string `json:"profession"`
+		School     string `json:"school"`
+		URL        string `json:"url"`
+	} `json:"source"`
+	Slug            string `json:"slug"`
+	RelativePath    string `json:"relativePath"`
+	WgaRelativePath string `json:"wgaRelativePath"`
+	WgaID           string `json:"wgaId"`
+	Meta            struct {
+		YearOfBirth          int    `json:"year_of_birth"`
+		PlaceOfBirth         string `json:"place_of_birth"`
+		YearOfDeath          int    `json:"year_of_death"`
+		PlaceOfDeath         string `json:"place_of_death"`
+		YearActiveStart      any    `json:"year_active_start"`
+		YearActiveEnd        any    `json:"year_active_end"`
+		ExactYearOfBirth     bool   `json:"exact_year_of_birth"`
+		ExactYearOfDeath     bool   `json:"exact_year_of_death"`
+		PlaceOfActivityStart string `json:"place_of_activity_start"`
+		PlaceOfActivityEnd   string `json:"place_of_activity_end"`
+		Normalized           string `json:"normalized"`
+		ExactActiveStart     any    `json:"exact_active_start"`
+		ExactActiveEnd       any    `json:"exact_active_end"`
+		KnownPlaceOfBirth    bool   `json:"known_place_of_birth"`
+		KnownPlaceOfDeath    bool   `json:"known_place_of_death"`
+		Parsed               bool   `json:"parsed"`
+	} `json:"meta"`
+	PossibleInfluences any    `json:"possibleInfluences"`
+	Bio                string `json:"bio"`
+}
 
 func init() {
 	m.Register(func(db dbx.Builder) error {
-		// add up queries...
+		dao := daos.New(db)
+
+		collection := &models.Collection{}
+
+		collection.Name = "artists"
+		collection.Type = models.CollectionTypeBase
+		collection.System = false
+		collection.MarkAsNew()
+		collection.Schema = schema.NewSchema(
+			&schema.SchemaField{
+				Id:          "artiust_name",
+				Name:        "name",
+				Type:        schema.FieldTypeText,
+				Options:     &schema.TextOptions{},
+				Presentable: true,
+			},
+			&schema.SchemaField{
+				Id:      "artist_slug",
+				Name:    "slug",
+				Type:    schema.FieldTypeText,
+				Options: &schema.TextOptions{},
+			},
+			&schema.SchemaField{
+				Id:      "artist_bio",
+				Name:    "bio",
+				Type:    schema.FieldTypeEditor,
+				Options: &schema.EditorOptions{},
+			},
+		)
+
+		err := dao.SaveCollection(collection)
+
+		if err != nil {
+			return err
+		}
+
+		// read the file at ../reference/glossary_stage_1.json
+		// unmarshal the json into a []Glossary
+		// loop through the []Glossary
+		// create a up query for each Glossary
+		// execute the up query
+
+		data, err := os.ReadFile("reference/artists_with_bio_stage_2.json")
+
+		if err != nil {
+			return err
+		}
+
+		var c []Artist
+
+		err = json.Unmarshal(data, &c)
+
+		if err != nil {
+			return err
+		}
+
+		for _, i := range c {
+			q := db.Insert("artists", dbx.Params{
+				"id":   i.Id,
+				"name": i.Name,
+				"bio":  i.Bio,
+				"slug": i.Slug,
+			})
+
+			_, err = q.Execute()
+
+			if err != nil {
+				return err
+			}
+
+		}
 
 		return nil
 	}, func(db dbx.Builder) error {
