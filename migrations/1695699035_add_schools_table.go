b2bad973a7f2fbd2fd26928d76e4adbd4eeaffd1
Meki <blackfyre@users.noreply.github.com>
2023-09-26 17:10:31 +0200

    Add schools table and update artists table

---
+++ migrations/1695699035_add_schools_table.go
@@ -0,0 +1,80 @@
+package migrations
+
+import (
+	"encoding/json"
+	"os"
+
+	"github.com/pocketbase/dbx"
+	"github.com/pocketbase/pocketbase/daos"
+	m "github.com/pocketbase/pocketbase/migrations"
+	"github.com/pocketbase/pocketbase/models"
+	"github.com/pocketbase/pocketbase/models/schema"
+)
+
+type School struct {
+	Id   string `json:"id"`
+	Name string `json:"name"`
+}
+
+func init() {
+	m.Register(func(db dbx.Builder) error {
+		dao := daos.New(db)
+
+		collection := &models.Collection{}
+
+		collection.Name = "schools"
+		collection.Id = "schools"
+		collection.Type = models.CollectionTypeBase
+		collection.System = false
+		collection.MarkAsNew()
+		collection.Schema = schema.NewSchema(
+			&schema.SchemaField{
+				Id:          "schools_name",
+				Name:        "name",
+				Type:        schema.FieldTypeText,
+				Options:     &schema.TextOptions{},
+				Presentable: true,
+			},
+		)
+
+		err := dao.SaveCollection(collection)
+
+		if err != nil {
+			return err
+		}
+
+		data, err := os.ReadFile("reference/schools.json")
+
+		if err != nil {
+			return err
+		}
+
+		var c []School
+
+		err = json.Unmarshal(data, &c)
+
+		if err != nil {
+			return err
+		}
+
+		for _, i := range c {
+			q := db.Insert("schools", dbx.Params{
+				"id":   i.Id,
+				"name": i.Name,
+			})
+
+			_, err = q.Execute()
+
+			if err != nil {
+				return err
+			}
+
+		}
+
+		return nil
+	}, func(db dbx.Builder) error {
+		// add down queries...
+
+		return nil
+	})
+}
