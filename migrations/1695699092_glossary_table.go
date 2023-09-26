aeb469e6dc8d9a0318034f80f8a378512379e723
Miklós Galicz <gnick666@gmail.com>
2023-09-26 15:01:30 +0200

    Add Glossary struct and initialize it with json data from reference file in glossary table migration

--- migrations/1695699092_glossary_table.go
+++ migrations/1695699092_glossary_table.go
@@ -1,18 +1,94 @@
 package migrations
 
 import (
+	"encoding/json"
+	"os"
+
 	"github.com/pocketbase/dbx"
+	"github.com/pocketbase/pocketbase/daos"
 	m "github.com/pocketbase/pocketbase/migrations"
+	"github.com/pocketbase/pocketbase/models"
+	"github.com/pocketbase/pocketbase/models/schema"
 )
+
+type Glossary struct {
+	Id         string `db:"id" json:"id"`
+	Expression string `db:"expression" json:"expression"`
+	Definition string `db:"definition" json:"definition"`
+}
 
 func init() {
 	m.Register(func(db dbx.Builder) error {
-		// add up queries...
+		dao := daos.New(db)
+
+		collection := &models.Collection{}
+
+		collection.Name = "glossary"
+		collection.Type = models.CollectionTypeBase
+		collection.System = false
+		collection.MarkAsNew()
+		collection.Schema = schema.NewSchema(
+			&schema.SchemaField{
+				Id:          "glossary_expression",
+				Name:        "expression",
+				Type:        schema.FieldTypeText,
+				Options:     &schema.TextOptions{},
+				Presentable: true,
+			},
+			&schema.SchemaField{
+				Id:      "glorssary_definition",
+				Name:    "definition",
+				Type:    schema.FieldTypeText,
+				Options: &schema.TextOptions{},
+			},
+		)
+
+		err := dao.SaveCollection(collection)
+
+		if err != nil {
+			return err
+		}
+
+		// read the file at ../reference/glossary_stage_1.json
+		// unmarshal the json into a []Glossary
+		// loop through the []Glossary
+		// create a up query for each Glossary
+		// execute the up query
+
+		data, err := os.ReadFile("reference/glossary_stage_1.json")
+
+		if err != nil {
+			return err
+		}
+
+		var glossary []Glossary
+
+		err = json.Unmarshal(data, &glossary)
+
+		if err != nil {
+			return err
+		}
+
+		for _, g := range glossary {
+			q := db.Insert("glossary", dbx.Params{
+				"id":         g.Id,
+				"expression": g.Expression,
+				"definition": g.Definition,
+			})
+
+			_, err = q.Execute()
+
+			if err != nil {
+				return err
+			}
+
+		}
 
 		return nil
 	}, func(db dbx.Builder) error {
-		// add down queries...
+		q := db.DropTable("glossary")
+		_, err := q.Execute()
 
-		return nil
+		return err
 	})
 }
