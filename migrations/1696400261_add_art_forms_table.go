503a720ea92d6ead629db27bf068398829f7c12f
Meki <blackfyre@users.noreply.github.com>
2023-10-04 08:22:17 +0200

    Add art_forms table and related migration

---
+++ migrations/1696400261_add_art_forms_table.go
@@ -0,0 +1,81 @@
+package migrations
+
+import (
+	"encoding/json"
+
+	"blackfyre.ninja/wga/assets"
+	"github.com/pocketbase/dbx"
+	"github.com/pocketbase/pocketbase/daos"
+	m "github.com/pocketbase/pocketbase/migrations"
+	"github.com/pocketbase/pocketbase/models"
+	"github.com/pocketbase/pocketbase/models/schema"
+)
+
+type ArtForm struct {
+	ID   string `json:"id"`
+	Name string `json:"name"`
+}
+
+func init() {
+	tName := "art_forms"
+	m.Register(func(db dbx.Builder) error {
+		dao := daos.New(db)
+
+		collection := &models.Collection{}
+
+		collection.Name = tName
+		collection.Type = models.CollectionTypeBase
+		collection.System = false
+		collection.MarkAsNew()
+		collection.Schema = schema.NewSchema(
+			&schema.SchemaField{
+				Id:          tName + "_name",
+				Name:        "name",
+				Type:        schema.FieldTypeText,
+				Options:     &schema.TextOptions{},
+				Presentable: true,
+			},
+		)
+
+		err := dao.SaveCollection(collection)
+
+		if err != nil {
+			return err
+		}
+
+		data, err := assets.InternalFiles.ReadFile("reference/forms.json")
+
+		if err != nil {
+			return err
+		}
+
+		var c []ArtForm
+
+		err = json.Unmarshal(data, &c)
+
+		if err != nil {
+			return err
+		}
+
+		for _, g := range c {
+			q := db.Insert(tName, dbx.Params{
+				"id":   g.ID,
+				"name": g.Name,
+			})
+
+			_, err = q.Execute()
+
+			if err != nil {
+				return err
+			}
+
+		}
+
+		return nil
+	}, func(db dbx.Builder) error {
+		q := db.DropTable(tName)
+		_, err := q.Execute()
+
+		return err
+	})
+}
