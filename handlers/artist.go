cd03e8716bf6da239182ad8e072d53899261a8f1
Miklós Galicz <gnick666@gmail.com>
2023-10-10 15:22:15 +0200

    Add artist details and artworks to handler functions

--- handlers/artist.go
+++ handlers/artist.go
@@ -16,13 +16,39 @@ func registerArtist(app *pocketbase.PocketBase) {
 		e.Router.GET("artists/:name", func(c echo.Context) error {
 			slug := c.PathParam("name")
 
+			artist, err := app.Dao().FindRecordsByFilter("artists", "slug = '"+slug+"'", "+name", 1, 0)
+
+			if err != nil {
+				return apis.NewNotFoundError("", err)
+			}
+
+			works, err := app.Dao().FindRecordsByFilter("artworks", "author = '"+artist[0].GetString("id")+"'", "+title", 100, 0)
+
+			if err != nil {
+				return apis.NewNotFoundError("", err)
+			}
+
+			data := map[string]any{
+				"Name":  artist[0].GetString("name"),
+				"Bio":   artist[0].GetString("bio"),
+				"Works": []map[string]string{},
+			}
+
+			for _, w := range works {
+				data["Works"] = append(data["Works"].([]map[string]string), map[string]string{
+					"Title":   w.GetString("title"),
+					"Comment": w.GetString("comment"),
+					"Image":   generateFileUrl(app, "artworks", w.GetString("id"), w.GetString("image")),
+					"Thumb":   generateThumbUrl(app, "artworks", w.GetString("id"), w.GetString("image"), "320x240"),
+				})
+			}
+
 			html := ""
-			err := error(nil)
 
 			if isHtmxRequest(c) {
-				html, err = renderBlock("artist:content", map[string]any{})
+				html, err = renderBlock("artist:content", data)
 			} else {
-				html, err = renderPage("artist", map[string]any{})
+				html, err = renderPage("artist", data)
 			}
 
 			if err != nil {
--- handlers/utils.go
+++ handlers/utils.go
@@ -2,7 +2,9 @@ package handlers
 
 import (
 	"fmt"
+	"strings"
 
+	"github.com/pocketbase/pocketbase"
 	"github.com/pocketbase/pocketbase/models"
 )
 
@@ -15,4 +17,22 @@ func normalizedBirthDeathActivity(record *models.Record) string {
 	End := record.GetInt("year_of_death")
 
 	return fmt.Sprintf("%d-%d", Start, End)
+}
+
+func generateFileUrl(app *pocketbase.PocketBase, collection string, collectionId string, fileName string) string {
+
+	endPoint := app.Settings().S3.Endpoint
+
+	endPoint = strings.Replace(endPoint, "https://", "https://"+app.Settings().S3.Bucket+".", 1)
+
+	return endPoint + "/" + collection + "/" + collectionId + "/" + fileName
+}
+
+func generateThumbUrl(app *pocketbase.PocketBase, collection string, collectionId string, fileName string, thumbSize string) string {
+
+	endPoint := app.Settings().S3.Endpoint
+
+	endPoint = strings.Replace(endPoint, "https://", "https://"+app.Settings().S3.Bucket+".", 1)
+
+	return endPoint + "/" + collection + "/" + collectionId + "/thumb_" + fileName + "/" + thumbSize + "_" + fileName
 }
