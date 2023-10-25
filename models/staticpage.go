0b1eef8c34647860e8fb8cc72e0fcc44679114d6
Meki <blackfyre@users.noreply.github.com>
2023-10-25 11:35:33 +0200

    Add comments and improve code readability in staticpage.go

--- models/staticpage.go
+++ models/staticpage.go
@@ -17,19 +17,23 @@ type StaticPage struct {
 
 var _ models.Model = (*StaticPage)(nil)
 
+// TableName returns the name of the collection associated with the StaticPage model.
 func (m *StaticPage) TableName() string {
-	return "static_pages" // the name of your collection
+	return "static_pages"
 }
 
+// StaticPageQuery returns a new dbx.SelectQuery for querying StaticPage models.
 func StaticPageQuery(dao *daos.Dao) *dbx.SelectQuery {
 	return dao.ModelQuery(&StaticPage{})
 }
 
+// FindStaticPageBySlug retrieves a StaticPage from the database by its slug.
+// It performs a case-insensitive match on the slug parameter.
+// Returns a pointer to the StaticPage and an error if any occurred.
 func FindStaticPageBySlug(dao *daos.Dao, slug string) (*StaticPage, error) {
 	page := &StaticPage{}
 
 	err := StaticPageQuery(dao).
-		// case insensitive match
 		AndWhere(dbx.NewExp("LOWER(slug)={:slug}", dbx.Params{
 			"slug": strings.ToLower(slug),
 		})).
