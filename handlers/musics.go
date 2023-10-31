b71c64d1e54e4d1f1ab1ad1eeb1424a3209444e5
Cardis950 <mihaly.galicz@zenosyne.tech>
2023-10-31 16:35:39 +0100

    WIP: migration relations and datagetter

--- handlers/musics.go
+++ handlers/musics.go
@@ -7,8 +7,10 @@ import (
 	"os"
 
 	"blackfyre.ninja/wga/assets"
+	shape "blackfyre.ninja/wga/models"
 	"blackfyre.ninja/wga/utils"
 	"github.com/labstack/echo/v5"
+	"github.com/pocketbase/dbx"
 	"github.com/pocketbase/pocketbase"
 	"github.com/pocketbase/pocketbase/apis"
 	"github.com/pocketbase/pocketbase/core"
@@ -45,7 +47,13 @@ func registerMusicHandlers(app *pocketbase.PocketBase) {
 			found := app.Cache().Has(cacheKey)
 
 			// TODO: implement data getter
-			musicList := GetMusics()
+			musicList, err := newGetComposers(app)
+			// musicList := GetMusics()
+
+			if err != nil {
+				fmt.Println("Error:", err)
+				return apis.NewNotFoundError("", err)
+			}
 
 			years := []string{}
 			for _, century := range musicList {
@@ -129,6 +137,97 @@ func GetMusics() []Century {
 		fmt.Println("Error unmarshalling JSON data:", err)
 	}
 
-	musicList := data
-	return musicList
+	return data
+}
+
+func GetParsedMusics([]Century) ([]shape.Composer) {
+	var composers []shape.Composer
+
+	for _, century := range GetMusics() {
+		for _, composer := range century.Composers {
+
+			songs := make([]shape.Song, len(composer.Songs))
+			for i, song := range composer.Songs {
+				songs[i] = shape.Song{
+					Title: song.Title,
+					URL: song.URL,
+					Source: song.Source,
+				}
+			}
+		
+			composers = append(composers, shape.Composer{
+				Name: composer.Name,
+				Date: composer.Date,
+				Language: composer.Language,
+				Century: century.Century,
+				Songs: songs,
+			})
+		}
+	}
+
+	return composers
+}
+
+func newGetComposers(app *pocketbase.PocketBase) ([]shape.Composer, error) {
+	// q := app.Dao().DB().NewQuery("SELECT * FROM music_composers INNER JOIN music_songs ON music_composers.name = music_songs.music_composer_name")
+
+	composers, err := app.Dao().FindRecordsByFilter(
+		"music_composers",
+		"id != null",
+		"+name",
+		0,
+		0,
+		dbx.Params{},
+	)
+
+	if err != nil {
+        return nil, err
+    }
+
+	songs, err := app.Dao().FindRecordsByFilter(
+		"music_songs",
+		"id != null",
+		"+title",
+		0,
+		0,
+		dbx.Params{},
+	)
+
+	if err != nil {
+        return nil, err
+    }
+
+	var data []shape.Composer
+
+	// i need to group songs by composer
+
+	for _, composer := range composers {
+		var songsByComposer []shape.Song
+		for _, song := range songs {
+			fmt.Println(song.GetString("music_composer_name"))
+			if song.GetString("music_composer_name") == composer.GetString("name") {
+				fmt.Println(song.GetString("title"))
+				songsByComposer = append(songsByComposer, shape.Song{
+					Title: song.GetString("title"),
+					URL: song.GetString("url"),
+					Source: song.GetStringSlice("source"),
+				})
+			}
+		}
+		data = append(data, shape.Composer{
+			Name: composer.GetString("name"),
+			Date: composer.GetString("date"),
+			Language: composer.GetString("language"),
+			Century: composer.GetString("century"),
+			Songs: songsByComposer,
+		})
+	}
+
+	for _, row := range data {
+		fmt.Println(row)
+	}
+
+	mockComposer := []shape.Composer{}
+
+    return mockComposer, nil
 }
