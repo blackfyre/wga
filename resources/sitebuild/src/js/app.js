afea9ae373dc6809a75151f67c63a4a30a23107c
Meki <blackfyre@users.noreply.github.com>
2023-11-02 12:22:01 +0100

    Update code formatting and dependencies

--- resources/sitebuild/src/js/app.js
+++ resources/sitebuild/src/js/app.js
@@ -1,19 +1,18 @@
-
-import * as bulmaToast from 'bulma-toast'
+import * as bulmaToast from "bulma-toast";
 
 const wga = {
-    els: {},
-    existingCloners: [],
-    dialogDefaultContent: '',
+  els: {},
+  existingCloners: [],
+  dialogDefaultContent: "",
 };
 
 bulmaToast.setDefaults({
-    duration: 5000,
-    position: 'top-right',
-    closeOnClick: true,
-    dismissible: true,
-    animate: { in: 'fadeIn', out: 'fadeOut' },
-})
+  duration: 5000,
+  position: "top-right",
+  closeOnClick: true,
+  dismissible: true,
+  animate: { in: "fadeIn", out: "fadeOut" },
+});
 
 wga.els.dialog = document.getElementById("d");
 wga.els.dialogDefaultContent = wga.els.dialog.innerHTML;
@@ -31,36 +30,34 @@ InitEventListeners();
  * @function
  * @returns {void}
  */
-function initNavbar () {
-    // Get all "navbar-burger" elements
-    const $navbarBurgers = document.querySelectorAll('.navbar-burger');
+function initNavbar() {
+  // Get all "navbar-burger" elements
+  const $navbarBurgers = document.querySelectorAll(".navbar-burger");
 
-    /**
-     * Toggles the "is-active" class on the target element and its corresponding button.
-     * @function
-     * @param {Event} event - The event object.
-     * @returns {void}
-     */
-    function classToggler (event) {
+  /**
+   * Toggles the "is-active" class on the target element and its corresponding button.
+   * @function
+   * @param {Event} event - The event object.
+   * @returns {void}
+   */
+  function classToggler(event) {
+    //find nearest `a` parent of event target
+    const target = event.target.closest("a") || event.target;
 
-        //find nearest `a` parent of event target
-        const target = event.target.closest('a') || event.target;
+    // Get the target from the "data-target" attribute
+    const dataTarget = target.dataset.target;
+    const $target = document.getElementById(dataTarget);
 
-        // Get the target from the "data-target" attribute
-        const dataTarget = target.dataset.target;
-        const $target = document.getElementById(dataTarget);
+    // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
+    target.classList.toggle("is-active");
+    $target.classList.toggle("is-active");
+  }
 
-        // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
-        target.classList.toggle('is-active');
-        $target.classList.toggle('is-active');
-
-    };
-
-    // Add a click event on each of them
-    $navbarBurgers.forEach(el => {
-        el.removeEventListener('click', classToggler);
-        el.addEventListener("click", classToggler);
-    });
+  // Add a click event on each of them
+  $navbarBurgers.forEach((el) => {
+    el.removeEventListener("click", classToggler);
+    el.addEventListener("click", classToggler);
+  });
 }
 
 /**
@@ -68,31 +65,30 @@ function initNavbar () {
  * @function
  * @returns {void}
  */
-function initViewer () {
-    const elements = document.querySelectorAll('[data-viewer]');
-    if (elements.length > 0) {
-        elements.forEach(element => {
-            new Viewer(element, {
-                toolbar: {
-                    zoomIn: 1,
-                    zoomOut: 1,
-                    oneToOne: 1,
-                    reset: 1,
-                    prev: 1,
-                    play: {
-                        show: 1,
-                        size: 'large',
-                    },
-                    next: 1,
-                    rotateLeft: 1,
-                    rotateRight: 1,
-                    flipHorizontal: 0,
-                    flipVertical: 0,
-                },
-            })
-        })
-    }
-
+function initViewer() {
+  const elements = document.querySelectorAll("[data-viewer]");
+  if (elements.length > 0) {
+    elements.forEach((element) => {
+      new Viewer(element, {
+        toolbar: {
+          zoomIn: 1,
+          zoomOut: 1,
+          oneToOne: 1,
+          reset: 1,
+          prev: 1,
+          play: {
+            show: 1,
+            size: "large",
+          },
+          next: 1,
+          rotateLeft: 1,
+          rotateRight: 1,
+          flipHorizontal: 0,
+          flipVertical: 0,
+        },
+      });
+    });
+  }
 }
 
 /**
@@ -101,49 +97,46 @@ function initViewer () {
  * @name initJumpToTop
  * @returns {void}
  */
-function initJumpToTop () {
-    const jumpToTop = document.querySelector('.jump.back-to-top');
-    if (jumpToTop) {
-        jumpToTop.addEventListener('click', () => {
-            window.scrollTo({ top: 0, behavior: 'smooth' });
-        })
-    }
+function initJumpToTop() {
+  const jumpToTop = document.querySelector(".jump.back-to-top");
+  if (jumpToTop) {
+    jumpToTop.addEventListener("click", () => {
+      window.scrollTo({ top: 0, behavior: "smooth" });
+    });
+  }
 }
 
+function DualModeListeners() {
+  const dualModeToggles = document.getElementsByClassName("toggle-dual");
+  const body = document.querySelector("body");
+  const dualModeSection = document.getElementById("dual");
 
-function DualModeListeners () {
-    const dualModeToggles = document.getElementsByClassName('toggle-dual');
-    const body = document.querySelector('body');
-    const dualModeSection = document.getElementById('dual');
+  if (dualModeToggles.length > 0) {
+    for (let i = 0; i < dualModeToggles.length; i++) {
+      //remove click event from all dual mode toggles
+      dualModeToggles[i].removeEventListener("click", (e) => {});
 
-    if (dualModeToggles.length > 0) {
-        for (let i = 0; i < dualModeToggles.length; i++) {
-
-            //remove click event from all dual mode toggles
-            dualModeToggles[i].removeEventListener('click', (e) => { });
-
-            dualModeToggles[i].addEventListener('click', (e) => {
-                e.preventDefault();
-
-            })
-        }
+      dualModeToggles[i].addEventListener("click", (e) => {
+        e.preventDefault();
+      });
     }
+  }
 }
 
-function ToggleDualMode () {
-    const html = document.querySelector('html');
-    const body = document.querySelector('body');
-    const dualModeSection = document.getElementById('dual');
+function ToggleDualMode() {
+  const html = document.querySelector("html");
+  const body = document.querySelector("body");
+  const dualModeSection = document.getElementById("dual");
 
-    if (body.classList.contains('has-active-backdrop')) {
-        html.classList.remove('is-clipped');
-        body.classList.remove('has-active-backdrop');
-        dualModeSection.classList.remove('is-active');
-    } else {
-        html.classList.add('is-clipped');
-        body.classList.add('has-active-backdrop');
-        dualModeSection.classList.add('is-active');
-    }
+  if (body.classList.contains("has-active-backdrop")) {
+    html.classList.remove("is-clipped");
+    body.classList.remove("has-active-backdrop");
+    dualModeSection.classList.remove("is-active");
+  } else {
+    html.classList.add("is-clipped");
+    body.classList.add("has-active-backdrop");
+    dualModeSection.classList.add("is-active");
+  }
 }
 
 /**
@@ -152,26 +145,26 @@ function ToggleDualMode () {
  * @name InitEventListeners
  * @returns {void}
  */
-function InitEventListeners () {
-    document.body.addEventListener("notification:toast", function (evt) {
-        if (evt.detail.closeDialog) {
-            wga.els.dialog.close();
-        }
+function InitEventListeners() {
+  document.body.addEventListener("notification:toast", function (evt) {
+    if (evt.detail.closeDialog) {
+      wga.els.dialog.close();
+    }
 
-        bulmaToast.toast({
-            message: evt.detail.message,
-            type: evt.detail.type,
-        })
-    })
-
-    document.body.addEventListener('htmx:load', function (evt) {
-        initViewer();
-        initCloner();
+    bulmaToast.toast({
+      message: evt.detail.message,
+      type: evt.detail.type,
     });
+  });
 
-    document.addEventListener("trix-before-initialize", () => {
-        Trix.config.toolbar.getDefaultHTML = () => {
-            return `
+  document.body.addEventListener("htmx:load", function (evt) {
+    initViewer();
+    initCloner();
+  });
+
+  document.addEventListener("trix-before-initialize", () => {
+    Trix.config.toolbar.getDefaultHTML = () => {
+      return `
     <div class="trix-button-row">
       <span class="trix-button-group trix-button-group--text-tools" data-trix-button-group="text-tools">
         <button type="button" class="trix-button trix-button--icon trix-button--icon-bold" data-trix-attribute="bold" data-trix-key="b" title="Bold" tabindex="-1">Bold</button>
@@ -184,66 +177,67 @@ function InitEventListeners () {
         <button type="button" class="trix-button trix-button--icon trix-button--icon-quote" data-trix-attribute="quote" title="Quote" tabindex="-1">Quote</button>
       </span>
 
-    </div>`
-        }  // Change Trix.config if you need
-    })
+    </div>`;
+    }; // Change Trix.config if you need
+  });
 }
 
-function initCloner () {
-    // find all the elements with data-cloner-target attribute
-    const cloners = document.querySelectorAll('[data-cloner-target]');
+function initCloner() {
+  // find all the elements with data-cloner-target attribute
+  const cloners = document.querySelectorAll("[data-cloner-target]");
 
-    // remove not seen items from wga.existingCloners
-    wga.existingCloners = wga.existingCloners.filter(el => document.body.contains(el));
+  // remove not seen items from wga.existingCloners
+  wga.existingCloners = wga.existingCloners.filter((el) =>
+    document.body.contains(el),
+  );
 
-    cloners.forEach(cloner => {
-        // get the target element
-        const target = document.querySelector(cloner.dataset.clonerTarget);
+  cloners.forEach((cloner) => {
+    // get the target element
+    const target = document.querySelector(cloner.dataset.clonerTarget);
 
-        // get the target's innetHTML as the template
-        const template = target.innerHTML;
+    // get the target's innetHTML as the template
+    const template = target.innerHTML;
 
-        // if target not in wga.existingCloners
+    // if target not in wga.existingCloners
 
-        if (!wga.existingCloners.includes(cloner.dataset.clonerTarget)) {
-            // add target to wga.existingCloners
-            wga.existingCloners.push(target);
+    if (!wga.existingCloners.includes(cloner.dataset.clonerTarget)) {
+      // add target to wga.existingCloners
+      wga.existingCloners.push(target);
 
-            cloner.addEventListener('click', () => {
-                // append the template to the target
-                target.insertAdjacentHTML('beforeend', template);
+      cloner.addEventListener("click", () => {
+        // append the template to the target
+        target.insertAdjacentHTML("beforeend", template);
 
-                // find all the elements with data-cloner-remove-me attribute
-                const removeMe = target.querySelectorAll('[data-cloner-remove-me]');
+        // find all the elements with data-cloner-remove-me attribute
+        const removeMe = target.querySelectorAll("[data-cloner-remove-me]");
 
-                // loop through each removeMe
+        // loop through each removeMe
 
-                removeMe.forEach(el => {
+        removeMe.forEach((el) => {
+          const removeMe = () => {
+            // find the closest .field element
+            const field = el.closest(".field");
 
-                    const removeMe = () => {
-                        // find the closest .field element
-                        const field = el.closest('.field');
+            // remove the field
+            field.remove();
+          };
 
-                        // remove the field
-                        field.remove();
-                    }
+          el.removeEventListener("click", removeMe);
 
-                    el.removeEventListener('click', removeMe);
-
-                    // add click event listener
-                    el.addEventListener('click', removeMe)
-                })
-            })
-        }
-    });
+          // add click event listener
+          el.addEventListener("click", removeMe);
+        });
+      });
+    }
+  });
 }
 
 window.wga = {
-    openDialog () {
-        wga.els.dialog.showModal();
-    },
-    closeDialog () {
-        wga.els.dialog.close();
-        wga.els.dialog.innerHTML = wga.els.dialogDefaultContent;
-    }
+  openDialog() {
+    wga.els.dialog.showModal();
+  },
+  closeDialog() {
+    wga.els.dialog.close();
+    wga.els.dialog.innerHTML = wga.els.dialogDefaultContent;
+  },
 };
