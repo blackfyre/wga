1bad16629a70929af40c98faebfb8e22273233c5
Meki <blackfyre@users.noreply.github.com>
2023-12-12 05:29:39 +0100

    Add GlossaryQuery and GetGlossaryItems functions

--- models/glossary.go
+++ models/glossary.go
@@ -1,6 +1,8 @@
 package models
 
 import (
+	"github.com/pocketbase/dbx"
+	"github.com/pocketbase/pocketbase/daos"
 	"github.com/pocketbase/pocketbase/models"
 )
 
@@ -14,4 +16,14 @@ var _ models.Model = (*GlossaryItem)(nil)
 
 func (m *GlossaryItem) TableName() string {
 	return "glossary" // the name of your collection
+}
+
+func GlossaryQuery(dao *daos.Dao) *dbx.SelectQuery {
+	return dao.ModelQuery(&GlossaryItem{})
+}
+
+func GetGlossaryItems(dao *daos.Dao) ([]*GlossaryItem, error) {
+	var c []*GlossaryItem
+	err := GlossaryQuery(dao).OrderBy("expression asc").All(&c)
+	return c, err
 }
