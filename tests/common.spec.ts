f3aff4e69629a0a329e0073d558db062b159337f
Galicz Miklós <blackfyre@users.noreply.github.com>
2024-03-11 08:43:59 +0100

    feat: Add test for artists page

--- tests/common.spec.ts
+++ tests/common.spec.ts
@@ -32,3 +32,28 @@ test("check feedback", async ({ page }) => {
   // expect notification popup: Thank you! Your feedback is valuable to us!
   await expect(page.locator(".is-success")).toBeVisible();
 });
+
+test("check artists page", async ({ page }) => {
+  await page.goto("/");
+
+  // Click the get started link.
+  await page.getByRole("link", { name: "Artists", exact: true }).click();
+
+  // expect to find "AACHEN, Hans von" on the page, in a table.
+
+  await expect(page.locator("table")).toHaveText(/AACHEN, Hans von/);
+
+  // use the search box to find "KOEDIJCK"
+  await page
+    .getByPlaceholder("Find an artist")
+    .pressSequentially("KOEDIJCK", { delay: 100 });
+
+  // expect to find "KOEDIJCK, Isaack" on the page, in a table.
+  await expect(page.locator("table")).toHaveText(/KOEDIJCK, Isaack/);
+
+  // follow the link KOEDIJCK, Isaack
+  await page.getByRole("link", { name: "KOEDIJCK, Isaack" }).click();
+
+  // expect to find "KOEDIJCK, Isaack" in the title.
+  await expect(page).toHaveTitle(/KOEDIJCK, Isaack/);
+});
