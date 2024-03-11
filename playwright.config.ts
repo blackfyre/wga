03e35a689f5749fb12e86f5b6a03bc30007be76c
Galicz Miklós <blackfyre@users.noreply.github.com>
2024-03-11 05:53:24 +0100

    ci: Cache Playwright binaries and optimize installation

--- playwright.config.ts
+++ playwright.config.ts
@@ -70,8 +70,8 @@ export default defineConfig({
 
   /* Run your local dev server before starting the tests */
   webServer: {
-    command: "npm run start",
-    url: "http://127.0.0.1:8090",
+    command: "wga serve",
+    url: "http://localhost:8090",
     reuseExistingServer: !process.env.CI,
   },
 });
