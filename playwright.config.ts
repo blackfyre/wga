e5aece8e298da5f0c59f1fc0adc75d3ccdc867f6
Galicz Miklós <blackfyre@users.noreply.github.com>
2024-03-12 08:29:35 +0100

    ci: Add shard strategy to Playwright tests and merge reports

--- playwright.config.ts
+++ playwright.config.ts
@@ -20,7 +20,7 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: [["github"], ["html"]],
+  reporter: process.env.CI ? "blob" : "html",
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
@@ -30,7 +30,6 @@ export default defineConfig({
     trace: "on-first-retry",
     screenshot: "only-on-failure",
     video: "retain-on-failure",
-    headless: process.env.CI ? true : false,
     viewport: { width: 1920, height: 1080 },
   },
 
@@ -38,7 +37,7 @@ export default defineConfig({
   projects: [
     {
       name: "chromium",
-      use: { ...devices["Desktop Chrome"], headless: true },
+      use: { ...devices["Desktop Chrome"] },
     },
 
     // {
