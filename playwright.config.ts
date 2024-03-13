408cbf32a3e77971425f2bcd1d225eaf7a778c4a
Galicz Miklós <blackfyre@users.noreply.github.com>
2024-03-13 08:35:36 +0100

    chore: comment out webServer configuration block

--- playwright.config.ts
+++ playwright.config.ts
@@ -73,9 +73,9 @@ export default defineConfig({
   ],
 
   /* Run your local dev server before starting the tests */
-  webServer: {
-    command: "./wga serve --dev",
-    url: "http://localhost:8090",
-    reuseExistingServer: !process.env.CI,
-  },
+  // webServer: {
+  //   command: "./wga serve --dev",
+  //   url: "http://localhost:8090",
+  //   reuseExistingServer: !process.env.CI,
+  // },
 });
