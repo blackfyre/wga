e0c0bdf104053e742c47a941316afbcbd3c8bfd9
Galicz Miklós <blackfyre@users.noreply.github.com>
2024-03-13 05:17:28 +0100

    style: update element id in feedback form

--- playwright.config.ts
+++ playwright.config.ts
@@ -22,6 +22,7 @@ export default defineConfig({
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: process.env.CI ? [["blob"], ["github"]] : "html",
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
+  globalTimeout: process.env.CI ? 4.8 * 60 * 1000 : 10 * 60 * 1000,
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
     baseURL: `${process.env.WGA_PROTOCOL}://${process.env.WGA_HOSTNAME}`,
