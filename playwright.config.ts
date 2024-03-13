3cd147f15629691cec9b6e570efffd8db493f0b8
Galicz Miklós <blackfyre@users.noreply.github.com>
2024-03-13 05:00:56 +0100

    feat: Update baseURL with dynamic environment variables

--- playwright.config.ts
+++ playwright.config.ts
@@ -24,7 +24,7 @@ export default defineConfig({
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
-    baseURL: "http://127.0.0.1:8090",
+    baseURL: `${process.env.WGA_PROTOCOL}://${process.env.WGA_HOSTNAME}`,
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: "on-first-retry",
