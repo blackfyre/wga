8ea9b3d829f5e6b1967e854d0c955e27f59898d3
Miklós Galicz <blackfyre@users.noreply.github.com>
2024-07-25 13:42:02 +0200

    refactor: :recycle: refactor static file handling with new public file system function

--- handlers/static.go
+++ handlers/static.go
@@ -2,13 +2,9 @@ package handlers
 
 import (
 	"context"
-	"embed"
-	"errors"
-	"fmt"
-	"net/url"
+	"io/fs"
+	"net/http"
 	"os"
-	"path/filepath"
-	"strings"
 
 	"github.com/blackfyre/wga/assets"
 	"github.com/blackfyre/wga/assets/templ/error_pages"
@@ -22,6 +18,19 @@ import (
 	"github.com/pocketbase/pocketbase/core"
 )
 
+// getFilePublicSystem returns an http.FileSystem that represents the public files system.
+// It retrieves the public files from the assets.PublicFiles and creates a sub file system.
+// It then returns the http.FS representation of the file system.
+func getFilePublicSystem() http.FileSystem {
+	fsys, err := fs.Sub(assets.PublicFiles, "public")
+
+	if err != nil {
+		panic(err)
+	}
+
+	return http.FS(fsys)
+}
+
 // registerStatic registers the static routes for the application.
 // It adds a middleware to serve static assets and a handler to serve static pages.
 // The static pages are retrieved from the database based on the slug parameter in the URL.
@@ -30,7 +39,8 @@ import (
 func registerStatic(app *pocketbase.PocketBase) {
 	app.OnBeforeServe().Add(func(e *core.ServeEvent) error {
 		// Assets
-		e.Router.GET("/assets/*", staticEmbeddedHandler(assets.PublicFiles))
+		assetHandler := http.FileServer(getFilePublicSystem())
+		e.Router.GET("/assets/*", echo.WrapHandler(http.StripPrefix("/assets/", assetHandler)))
 
 		// Sitemap
 		e.Router.GET("/sitemap/*", apis.StaticDirectoryHandler(os.DirFS("./wga_sitemap"), false))
@@ -71,31 +81,4 @@ func registerStatic(app *pocketbase.PocketBase) {
 
 		return nil
 	})
-}
-
-// staticEmbeddedHandler returns an echo.HandlerFunc that serves static files embedded in the given embed.FS.
-// The function takes a context object and returns an error. It first unescapes the URL path and then constructs
-// the file path by cleaning and trimming the path parameter. If the file exists, it is served using the echo.Context's
-// FileFS method. If the file does not exist, the function serves the 404.html file from the public directory.
-func staticEmbeddedHandler(embedded embed.FS) echo.HandlerFunc {
-	return func(c echo.Context) error {
-		p := c.PathParam("*")
-
-		// escape url path
-		tmpPath, err := url.PathUnescape(p)
-		if err != nil {
-			return fmt.Errorf("failed to unescape path variable: %w", err)
-		}
-		p = tmpPath
-
-		name := "public/" + filepath.ToSlash(filepath.Clean(strings.TrimPrefix(p, "/")))
-
-		fileErr := c.FileFS(name, embedded)
-
-		if fileErr != nil && errors.Is(fileErr, echo.ErrNotFound) {
-			return c.Redirect(404, "/error_404")
-		}
-
-		return fileErr
-	}
 }
