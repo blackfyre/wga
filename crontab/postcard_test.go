8f434410a883a8cf0a85b3cc86112362ec3d228d
Miklós Galicz <blackfyre@users.noreply.github.com>
2024-07-29 14:08:25 +0200

    feat: :sparkles: add new test for sending email with HTML content

---
+++ crontab/postcard_test.go
@@ -0,0 +1,42 @@
+package crontab
+
+import (
+	"fmt"
+	"net/mail"
+	"testing"
+
+	"github.com/pocketbase/pocketbase"
+	"github.com/pocketbase/pocketbase/tools/mailer"
+)
+
+func TestSendMail(t *testing.T) {
+
+	app := pocketbase.NewWithConfig(pocketbase.Config{
+		DefaultDataDir: "./wga_data",
+	})
+
+	mailClient := app.NewMailClient()
+
+	fmt.Printf("Mail client: %v\n", mailClient)
+
+	message := &mailer.Message{
+		From: mail.Address{
+			Name:    "sender",
+			Address: "sender@example.com",
+		},
+		To: []mail.Address{
+			{
+				Name:    "recipient",
+				Address: "recipient@example.com",
+			},
+		},
+		Subject: "Test Subject",
+		HTML:    "<html><body>Test Body</body></html>",
+	}
+
+	err := mailClient.Send(message)
+	if err != nil {
+		t.Errorf("sendMail returned an error: %v", err)
+	}
+
+}
