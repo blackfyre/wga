fa147cef641b2df96474cc0a0b97eff7fd5271d1
Miklós Galicz <blackfyre@users.noreply.github.com>
2025-02-05 06:00:46 +0100

    chore: refactor DTOs by separating Image and GuestbookEntry types

--- assets/templ/dto/dto.go
+++ assets/templ/dto/dto.go
@@ -1,20 +1,5 @@
 package dto
 
-type Image struct {
-	Thumb     string
-	Image     string
-	Title     string
-	Technique string
-	Comment   string
-	Url       string
-	Id        string
-	Jsonld    interface{}
-	HxTarget  string
-	Artist
-}
-
-type ImageGrid []Image
-
 type Artist struct {
 	Id         string
 	Name       string
