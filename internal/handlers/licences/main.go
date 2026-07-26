2f28617a5266e713d117c4e3de01432fbd4c48b5
Miklós Galicz <1991410+blackfyre@users.noreply.github.com>
2026-07-26 10:38:58 +0200

    feat: generate licence notices and SBOM

---
+++ internal/handlers/licences/main.go
@@ -0,0 +1,33 @@
+package licences
+
+import (
+	"bytes"
+	"net/http"
+
+	"github.com/blackfyre/wga/internal/assets"
+	"github.com/blackfyre/wga/internal/assets/templ/pages"
+	tmplUtils "github.com/blackfyre/wga/internal/assets/templ/utils"
+	"github.com/blackfyre/wga/internal/utils"
+	"github.com/pocketbase/pocketbase"
+	"github.com/pocketbase/pocketbase/core"
+)
+
+func RegisterHandlers(app *pocketbase.PocketBase) {
+	app.OnServe().BindFunc(func(se *core.ServeEvent) error {
+		se.Router.GET("/open-source-licences", func(c *core.RequestEvent) error {
+			ctx := tmplUtils.DecorateContext(c.Request.Context(), tmplUtils.TitleKey, "Open-source licences")
+			ctx = tmplUtils.DecorateContext(ctx, tmplUtils.DescriptionKey, "Third-party software included in the Web Gallery of Art.")
+			ctx = tmplUtils.DecorateContext(ctx, tmplUtils.CanonicalUrlKey, tmplUtils.AssetUrl("/open-source-licences"))
+			c.Response.Header().Set("HX-Push-Url", utils.GenerateCurrentRelativePageUrl(c))
+
+			var buffer bytes.Buffer
+			if err := pages.LicencesPage(assets.OpenSourceLicencesHTML).Render(ctx, &buffer); err != nil {
+				return utils.ServerFaultError(c)
+			}
+
+			return c.HTML(http.StatusOK, buffer.String())
+		})
+
+		return se.Next()
+	})
+}
